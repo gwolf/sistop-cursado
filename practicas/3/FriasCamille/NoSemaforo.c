f50a6c9e698751d97f6a25565adbfb847eeedfeb
FriasCamille <camfriher@comunidad.unam.mx>
2023-03-23 01:02:12 -0600

    Debí compilar antes pero ahora sí ya funciona de verdad

--- practicas/3/FriasCamille/NoSemaforo.c
+++ practicas/3/FriasCamille/NoSemaforo.c
@@ -7,51 +7,69 @@ void main ()
 	int semaforo, opc=0;
 	semaforo=init();
 	
-	while (opc!=3)
+	do
 	{
-		printf("\n \t Que desea hacer:_");
-		printf("\n \t 1)... aquiere \n \t 2)... realese \n \t 3)... salir" );
-		sacnf("%i",opc)
+		
 		switch(opc)
 		{
+			case 0:
+				printf("\n \t Que desea hacer:_");
+				printf("\n \t 1)... aquiere \n \t 2)... realese \n \t 3)... salir \n \t _" );
+				scanf("%i",&opc);
+				break;
 			case 1:
-				aquiere(semaforo);
+				semaforo=aquiere(semaforo);
+				opc=0;
+				if (semaforo != 0)
+				{
+					printf("\n \t Estoy libre no te preocupes");
+				}
+				else
+				{
+					printf("\n \t Ya me ocupe no molestes");
+				}
+				break;
 			case 2:
-				realese(semaforo);
+				semaforo=realese(semaforo);
+				opc=0;
+				if (semaforo != 0)
+				{
+					printf("\n \t Estoy libre no te preocupes");
+				}
+				else
+				{
+					printf("\n \t Ya me ocupe no molestes");
+				}
+				break;
 			case 3: 
 				printf("\n \t gracias por ver el ejemplo");
+				break;
+			default: printf("\n \t Esa no es una opcion");
 		}
-		if (semaforo != 0)
-		{
-			printf("\n \t Estoy libre no te preocupes");
-		}
-		else
-		{
-			printf("\n \t Ya me ocupe no molestes");
-		}
-	}
+		
+	}while (opc!=3);
 	
 }
 
 int init ()
 {
-	int inicio;
+	int inicio=0;
 	printf("\n \t Ingrese el valor inicial del semaforo:_");
-	scanf("%i", inicio);
+	scanf("%i", &inicio);
 	printf("\n \t El valor inicial del semaforo es: %i", inicio);
 	
 	return inicio;
 }
 int aquiere(int semaforo)
 {
-	semaforo--;
-	return 0;
+	semaforo=semaforo-1;
+	return semaforo;
 }
 
 int realese(int semaforo)
 {
-	semaforo++;
-	return 0;
+	semaforo=semaforo+1;
+	return semaforo;
 }
 
 
