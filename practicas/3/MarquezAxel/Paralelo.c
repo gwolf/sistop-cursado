cc08376adf4da495186d4cdb359f202edaffd0e8
RowXV <daxel.02.04@gmail.com>
2023-03-22 20:18:58 -0600

    Programa paralelo

---
+++ practicas/3/MarquezAxel/Paralelo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	
+    #pragma omp parallel
+{
+	int i;
+	printf("Hola mundo \n");
+   for(i=0;i<10;i++)
+    printf("Iteracion: %d\n", i);
+}
+
+printf("Adios\n");
+return 0;
+}
