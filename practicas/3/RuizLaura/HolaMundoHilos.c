1275245265a137a15619c64840298a9adf31599a
Laura Ruiz <ruizfloreslauraandrea@gmail,com>
2023-03-23 23:27:19 -0600

    Agrego archivo de Hola Mundo con uso de hilos

---
+++ practicas/3/RuizLaura/HolaMundoHilos.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main(){
+  int tid,nth;
+  #pragma omp parallel
+  {
+    tid = omp_get_thread_num();
+    nth = omp_get_num_threads();
+    printf("Hola mundo desde el hilo %d de un total de %d\n",tid,nth);
+  }
+  printf("Adios");
+  return 0;
+}
